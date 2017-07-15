ab8f37c49ebeec60ffe4b634468523cf1801b1d9
NASA\E9939461 <josephpboyle@eaton.com>
2017-07-15 01:24:13 -0400

    modified project expander and removed email form

--- src/app/social-button.component.ts
+++ src/app/social-button.component.ts
@@ -3,7 +3,7 @@ import {Component, Input} from '@angular/core';
 @Component({
 	selector: 'social-button',
 	template:`
-		<div class="socialButton" [style.background-image]="imageUrl" (click)="openLink()">
+		<div class="socialButton" [title]="linkUrl" [style.background-image]="imageUrl" (click)="openLink()">
 			<div class="lighten"></div>
 		</div>
 	`,
@@ -14,6 +14,11 @@ export class SocialButtonComponent{
 	@Input() imageUrl: string='';
 	@Input() linkUrl: string='';
 	public openLink(){
-	    window.open(this.linkUrl, "_blank");
+		if(this.linkUrl.match(/^mailto:/)){
+			location.href = this.linkUrl;
+		}
+		else{
+	    	window.open(this.linkUrl, "_blank");
+		}
 	};
 }
