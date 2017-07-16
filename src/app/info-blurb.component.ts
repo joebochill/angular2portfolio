20a66a2a6bc03a793339f34808c94f376c50d6a2
NASA\E9939461 <josephpboyle@eaton.com>
2017-07-15 21:46:11 -0400

    finished implementing expanders for projects

--- src/app/info-blurb.component.ts
+++ src/app/info-blurb.component.ts
@@ -1,4 +1,5 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, ElementRef} from '@angular/core';
+const transition = require('transition-to-from-auto');
 
 @Component({
 	selector: 'info-blurb',
@@ -11,7 +12,16 @@ export class InfoBlurbComponent{
 	@Input() data: Object = {};
 	@Input() expanded: boolean=false;
 
+	constructor(private elementRef: ElementRef){}
+
 	private toggle(){
 	    this.expanded = !this.expanded;
+	    var elem = this.elementRef.nativeElement.getElementsByClassName("detailsWrapper")[0];
+	    transition({element: elem, val: (this.expanded?"auto":"0px")});
+	    transition({
+		  element: elem,
+		  prop: "margin-top",
+		  val: (this.expanded?"1rem":"0px")
+		});
 	};
 }
