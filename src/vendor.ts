8dd9f2ef75b4d496f1b888fd9a9673ea76721cb8
NASA\E9939461 <josephpboyle@eaton.com>
2017-07-19 10:10:19 -0400

    optimizations

---
+++ src/vendor.ts
@@ -0,0 +1,13 @@
+// Angular
+import '@angular/platform-browser';
+import '@angular/platform-browser-dynamic';
+import '@angular/core';
+import '@angular/common';
+import '@angular/http';
+
+// RxJS
+import 'rxjs';
+
+// other libraries
+import 'animated-scroll-to';
+import 'transition-to-from-auto';
